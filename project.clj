4626a893a70cca0e216ba3838ae553b12a954666
Constantine Vetoshev <vetoshev@gmail.com>
2014-08-07 21:18:13 -0700

    Version bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject envvar "1.0.0-SNAPSHOT"
+(defproject envvar "1.0.0"
   :url "http://github.com/gcv/envvar"
   :description "Clojure library for handling environment variables"
   :license {:name "Eclipse Public License"
