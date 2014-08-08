6988b3277a66d111ff8f619c95b89f3f60f7d43d
Constantine Vetoshev <vetoshev@gmail.com>
2014-08-07 21:05:50 -0700

    Documentation and description.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject envvar "1.0.0-SNAPSHOT"
   :url "http://github.com/gcv/envvar"
+  :description "Clojure library for handling environment variables"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
