251c300e527798945a978d8bba5af07d3fd4f85f
Constantine Vetoshev <vetoshev@gmail.com>
2020-10-27 21:12:46 -0700

    Update Clojure dependency; update license to EPL v2; bump version.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject envvar "1.1.0"
+(defproject envvar "1.1.1"
   :url "http://github.com/gcv/envvar"
   :description "Clojure library for handling environment variables"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :license {:name "EPL-2.0" :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]])
