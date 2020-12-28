7afd060060207cced20b025bb61c24e5e3cad632
Constantine Vetoshev <vetoshev@gmail.com>
2020-12-27 22:52:45 -0800

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject envvar "1.1.1"
+(defproject envvar "1.1.2"
   :url "http://github.com/gcv/envvar"
   :description "Clojure library for handling environment variables"
   :license {:name "EPL-2.0" :url "https://www.eclipse.org/legal/epl-2.0/"}
